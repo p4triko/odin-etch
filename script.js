5cb560e5d77cb2a8897a463884e3786b5ff64d8c
p4triko <168976850+p4triko@users.noreply.github.com>
2024-06-05 16:21:05 +0300

    Add eventlistener for background color change, ignore resetbutton sections for now

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const gridBox = document.getElementById("container");
 const gridDimensions = 700; // Grid height and width, square, from css
 
-// const gridSize = prompt("Enter a number between 1 and 100 for your grid size");
+const resetButton = document.querySelector(".size-button");
+
+// resetButton.addEventListener("click", () => {
+//   // createGrid();
+// });
 
 createGrid(16);
 
@@ -12,13 +16,30 @@ function createGrid(num) {
     for (let j = 0; j < num; j++) {
       const Dimensions = gridDimensions / num;
       const divColumns = document.createElement("divColumns");
-      divColumns.classList.add("div-columns");
 
+      divColumns.classList.add("div-columns");
       divColumns.style.width = `${Dimensions}px`;
       divColumns.style.height = `${Dimensions}px`;
 
       divRows.append(divColumns);
+
+      divColumns.addEventListener("mouseenter", () => {
+        divColumns.style.backgroundColor = "#000000";
+      });
     }
     gridBox.appendChild(divRows);
   }
 }
+
+// const gridSize = prompt("Enter a number between 1 and 100 for your grid size");
+
+// function resetGrid() {
+//   const gridSize = prompt(
+//     "Enter a number between 1 and 100 for your grid size"
+//   );
+//   if (gridSize < 1 && gridSize > 100) {
+//     resetGrid();
+//   } else {
+//     return gridSize;
+//   }
+// }
